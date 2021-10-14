bd8ee4dee9538a0bca055afbc0f3732c9f2f1e7e
Humpers96 <alex.humphrey@hotmail.co.uk>
2021-10-14 20:48:21 +0100

    added placeholder handlebars site

--- routes/api/api.js
+++ routes/api/api.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.status(200).send("API up and running...")
+    res.send("API up and running...")
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,20 +2,35 @@
 const express = require('express')
 const app = express()
 
+// Path setup
+const path = require('path')
+
 // Routes setup
 const api_routes = require('./routes/api/api')
 const website_routes = require('./routes/website/website')
 
-// PORT
-const port = process.env.PORT || 4237
+// Static folder setup
+app.use(express.static(path.join(__dirname,'public')))
+
+// Handlbars setup
+const hbs = require('express-handlebars')
+app.set('view engine', 'handlebars')
+app.engine('handlebars', hbs(
+    {
+        defaultLayout: "index"
+    }
+))
 
 // Landing page server status response
 app.get('/', (req, res) => {
-    res.send('Server online.')
+    res.render('nothing_here')
 })
 
 // Exposing my API
 app.use('/api', api_routes)
+
+// PORT
+const port = process.env.PORT || 4237
 
 // Run server
 app.listen(port, () => {
